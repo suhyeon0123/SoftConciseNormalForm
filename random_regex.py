e7002496145678b8c7932cecc3998c3feee0e231
suhyeon0123 <tngus98207@naver.com>
2021-01-26 23:37:41 +0900

    make random regex

--- random_regex.py
+++ random_regex.py
@@ -1,11 +1,12 @@
-from parsetree import*
+from parsetree_prune import*
+from xeger import Xeger
+import re2 as re
 
-limit = 10
+limit = 5
 
-'''regex = RE(Concatenate(KleenStar(Character('0')),Hole()))
-print(regex)
-regex.unroll()
-print(regex)'''
+
+
+
 
 
 
@@ -13,9 +14,65 @@ print(regex)'''
 def rand_example(limit):
     regex = RE()
     for count in range(limit):
-        regex.make_child()
+        regex.make_child(1)
     regex.spreadRand()
     return regex
 
-print(rand_example(limit))
+'''regex = rand_example(limit)
+print(regex)
+x = Xeger()
+posset = set()
+for i in range(0,10):
+    posset.add(x.xeger(repr(regex)))
+for index, i in enumerate(posset):
+    print(i)
+exit()'''
+
+
+for no in range(1, 1000):
+    regex = rand_example(limit)
+
+    x = Xeger()
+    posset = set()
+    for i in range(0, 15):
+        posset.add(x.xeger(repr(regex)))
+
+    negset = set()
+    for i in range(0, 1000):
+        # random regex생성
+        str_list = []
+
+        for j in range(0, random.randrange(1, 15)):
+            if random.random() < 0.5:
+                str_list.append('0')
+            else:
+                str_list.append('1')
+        tmp = ''.join(str_list)
+
+        # random regex가 맞지 않다면 추가
+        if not bool(re.fullmatch(repr(regex), tmp)):
+            negset.add(tmp + "\n")
+
+        if len(negset) == 15:
+            break
+
+    if not negset:
+        continue
+
+    fname = "rand_benchmarks/no" + str(no) + ".txt"
+    f = open(fname, 'wt')
+
+    f.write(repr(regex)+"\n")
+
+    f.write('++\n')
+
+    for index, i in enumerate(posset):
+        if i!= '' : f.write(i+"\n")
+
+    f.write('--\n')
+
+    for index, i in enumerate(negset):
+        if i!= '' : f.write(i)
+
+    f.close()
 
